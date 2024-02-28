a15edc9fd4b135d71d08780afe02d5ea862b681f
Joan Cifo <joancifo@gmail.com>
2024-02-28 13:47:11 +0100

    wip

--- frontend/components/StepHandler.vue
+++ frontend/components/StepHandler.vue
@@ -1,36 +1,44 @@
 <script setup>
 import { ref, onBeforeMount, onMounted } from "vue";
+import WrapperTablet from "~/components/WrapperTablet.vue";
 
 const props = defineProps({
   step: {
     type: Number,
-    required: true
-  }
+    required: true,
+  },
 });
 
 // Define los eventos que el componente puede emitir
-const emit = defineEmits(['changeStep']);
+const emit = defineEmits(["changeStep"]);
 
 // Función para emitir el evento changeStep
 function emitChangeStep(step) {
-  emit('changeStep', step);
+  emit("changeStep", step);
 }
-
 </script>
 
 <template>
   <div>
-    <slot></slot>
-    <button v-if="step !== 5" @click="emitChangeStep(step + 1)">Cambiar al siguiente paso</button>
-    <button v-if="step === 5" @click="emitChangeStep(1)">Volver al Paso 1</button>
+    <WrapperTablet v-if="step === 1"> CONTENIDO 1 </WrapperTablet>
+    <WrapperTablet v-if="step === 2"> CONTENIDO 2 </WrapperTablet>
+    <WrapperTablet v-if="step === 3"> CONTENIDO 3 </WrapperTablet>
+    <WrapperTablet v-if="step === 4"> CONTENIDO 4 </WrapperTablet>
+    <WrapperTablet v-if="step === 5"> CONTENIDO 5 </WrapperTablet>
+
+    <button v-if="step !== 5" @click="emitChangeStep(step + 1)">
+      Cambiar al siguiente paso
+    </button>
+    <button v-if="step === 5" @click="emitChangeStep(1)">
+      Volver al Paso 1
+    </button>
   </div>
 </template>
 
 <style lang="scss" scoped>
-
 $verde: #00ce7e;
-$rojo: #D41C24;
-$naranja: #E2973B;
+$rojo: #d41c24;
+$naranja: #e2973b;
 .mainWrapper {
   width: 800px;
   height: 1280px;
@@ -40,5 +48,4 @@ $naranja: #E2973B;
     height: 800px;
   }
 }
-
 </style>
---
+++ frontend/components/WrapperTablet.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+<template>
+  <div class="wrapper">
+    <slot></slot>
+  </div>
+</template>
+<style lang="scss" scoped>
+// cosas comunes
+.wrapper {
+  width: 800px;
+  background-color: red;
+}
+</style>
