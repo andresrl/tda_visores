482112b7a863a60059e8b21471d2f46a0ed90290
Andrés <andres@arleria.com>
2024-03-03 15:29:01 +0100

    wip
    Signed-off-by: Andrés <andres@arleria.com>

--- frontend/components/Tablet/StepHandler.vue
+++ frontend/components/Tablet/StepHandler.vue
@@ -166,10 +166,28 @@ const postMeetingData = async () => {
     professional_sector: professionalCompanyType.value,
   };
 
-  const { data } = await $fetch(`${runtimeConfig.public.NODE_SERVER_URL}/api/save-meeting-json`, {
-    method: "POST",
-    body: JSON.stringify(body),
+  // const { data } = await $fetch(`${runtimeConfig.public.NODE_SERVER_URL}/api/save-meeting-json`, {
+  // const { data } = await $fetch(`${runtimeConfig.public.BACKEND_SERVER_URL}/`, {
+  //   method: "POST",
+  //   body: JSON.stringify(body),
+  // });
+
+
+  const params = new URLSearchParams({
+    company_name: body.company_name,
+    company_tradename: body.company_tradename,
+    company_username: body.company_username,
+    company_email: body.company_email,
+    professional_fullname: body.professional_fullname,
+    professional_company: body.professional_company,
+    professional_email: body.professional_email,
+    professional_sector: body.professional_sector
+  }).toString();
+
+  const { data } = await $fetch(`${runtimeConfig.public.BACKEND_SERVER_URL}/?${params}`, {
+    method: "GET"
   });
+
   alert("DATA: " + data);
   console.log(data);
 };
